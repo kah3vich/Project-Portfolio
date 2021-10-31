65daef4d18809eed3201c06f3a57cc6dfa7c786e
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-10-31 22:18:28 +0300

    file

--- public/mail.php
+++ public/mail.php
@@ -13,13 +13,13 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'bot.circle@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'YRUe+oiAts44'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = ''; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = ''; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('bot.circle@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('nik.b20@mail.ru');     // Кому будет уходить письмо 
+$mail->setFrom(''); // от кого будет уходить письмо?
+$mail->addAddress('');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
